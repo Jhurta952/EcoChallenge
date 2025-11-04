53ab78f4406bbca9abcbcb6cdb8cccbcbccb8203
Jhurta952 <camachohurtado3@gmail.com>
2025-11-04 18:28:08 -0500

    Se impliementa UsuariosLog la cual se encarga de validar los inicios de seccion

---
+++ LogicaNegocio/UsuarioLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using AccesoDatos;
+
+namespace LogicaNegocio
+{
+    public class UsuarioLog
+    {
+        private UsuariosDat usuariosDal = new UsuariosDat();
+
+        public Usuario IniciarSesion(string correo, string contraseña)
+        {
+            return usuariosDal.ObtenerUsuarioPorCorreoYContraseña(correo, contraseña);
+        }
+    }
+}
