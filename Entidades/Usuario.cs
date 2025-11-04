f158bb9554c563bb54a0e4ae2b4f1e44e0403de6
Jhurta952 <camachohurtado3@gmail.com>
2025-11-04 18:14:51 -0500

    Agregada clase Usuario en la capa Entidades

---
+++ Entidades/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+        public string Rol { get; set; }
+
+        public Usuario(int id, string nombre, string correo, string contraseña, string rol)
+        {
+            Id = id;
+            Nombre = nombre;
+            Correo = correo;
+            Contraseña = contraseña;
+            Rol = rol;
+        }
+
+
+    }
+}
