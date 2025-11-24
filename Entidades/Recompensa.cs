076ae0b6f5866221e80712ca3c2d3fa0cd053d56
Jhurta952 <camachohurtado3@gmail.com>
2025-11-24 16:56:27 -0500

    Se crea la clase Recompensa Para las medallas

---
+++ Entidades/Recompensa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Recompensa
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int PuntosRequeridos { get; set; }
+
+        public Recompensa() {}
+
+        public Recompensa(int id, string nombre, string descripcion, int puntosRequeridos)
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            PuntosRequeridos = puntosRequeridos;
+        }
+    }
+}
