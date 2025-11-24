5c18ae6ba16425f22b9a19717fa76dfec9183fb8
Jhurta952 <camachohurtado3@gmail.com>
2025-11-24 17:00:48 -0500

    Se crea la clase HistorialMision para guardar el progreso

---
+++ Entidades/HistorialMision.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class HistorialMision
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public int IdMision { get; set; }
+        public DateTime FechaComplecion { get; set; }
+
+        public HistorialMision() { }
+
+        public HistorialMision(int id, int idUsuario, int idMision, DateTime fechaComplecion)
+        {
+            Id = id;
+            IdUsuario = idUsuario;
+            IdMision = idMision;
+            FechaComplecion = fechaComplecion;
+        }
+
+
+    }
+}
--- Entidades/Recompensa.cs
+++ Entidades/Recompensa.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    internal class Recompensa
+    public class Recompensa
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
