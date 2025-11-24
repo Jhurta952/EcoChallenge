b2991e5ac1b600838b385cd8db6ea230b14dd848
Jhurta952 <camachohurtado3@gmail.com>
2025-11-24 16:51:23 -0500

    Se agrega la clase Mision para las tareas ecologicas

--- Entidades/Mision.cs
+++ Entidades/Mision.cs
@@ -8,5 +8,19 @@ namespace Entidades
 {
     public class Mision
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Puntos { get; set; }
+
+        public Mision() { }
+
+        public Mision(int id, string nombre, string descripcion, int puntos)
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Puntos = puntos;
+        }
     }
 }
